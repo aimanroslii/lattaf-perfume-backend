adf30d75722f076ccc537ac7108bd585ab96ca0b
Aiman R. <muhammadaimanrosli11@gmail.com>
2025-10-10 23:00:36 +0800

    Implemented circuit breaker
    Signed-off-by: Aiman R. <muhammadaimanrosli11@gmail.com>

---
+++ api-gateway/src/main/java/com/lattaf/perfumes/gateway/config/CorsGlobalConfig.java
@@ -0,0 +1,28 @@
+package com.lattaf.perfumes.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+public class CorsGlobalConfig {
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowedOriginPatterns(List.of("http://localhost:4200"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+        config.setAllowCredentials(true); // needed if using Authorization header
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return source;
+    }
+}
--- product-service/src/main/java/com/lattaf/perfumes/product/dto/ProductRequest.java
+++ product-service/src/main/java/com/lattaf/perfumes/product/dto/ProductRequest.java
@@ -5,8 +5,10 @@ import lombok.Builder;
 import java.math.BigDecimal;
 
 public record ProductRequest(
+       String id,
        String name,
        String description,
+       String skuCode,
        BigDecimal price
 ) {
 }
