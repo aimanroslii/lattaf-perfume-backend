636ca9c6154c4eb8289fea2cf8e071b71ae56687
Aiman R. <muhammadaimanrosli11@gmail.com>
2025-10-11 16:34:35 +0800

    Created notification service and enable event driven arc using Kafka
    Signed-off-by: Aiman R. <muhammadaimanrosli11@gmail.com>

--- order-service/src/main/java/com/lattaf/perfumes/order/service/OrderService.java
+++ order-service/src/main/java/com/lattaf/perfumes/order/service/OrderService.java
@@ -2,12 +2,14 @@ package com.lattaf.perfumes.order.service;
 
 import com.lattaf.perfumes.order.client.InventoryClient;
 import com.lattaf.perfumes.order.dto.OrderRequest;
+import com.lattaf.perfumes.order.event.OrderPlacedEvent;
 import com.lattaf.perfumes.order.model.Order;
 import com.lattaf.perfumes.order.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.kafka.core.KafkaTemplate;
 
 import java.util.UUID;
 
@@ -19,21 +21,29 @@ public class OrderService {
 
     private final OrderRepository orderRepository;
     private final InventoryClient inventoryClient;
+    private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
 
     public void placeOrder(OrderRequest orderRequest){
         var isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
-        // Map OrderRequest to Order object
+
         if(isProductInStock){
             Order order = new Order();
             order.setOrderNumber(UUID.randomUUID().toString());
             order.setSkuCode(orderRequest.skuCode());
             order.setPrice(orderRequest.price());
             order.setQuantity(orderRequest.quantity());
-            //var order = mapToOrder(orderRequest);
-
-            // Save order to OrderRepository
             orderRepository.save(order);
             log.info("Order placed successfully with order number: {}", order.getOrderNumber());
+
+            // Send the message to Kafka Topic
+            OrderPlacedEvent orderPlacedEvent = new OrderPlacedEvent();
+            orderPlacedEvent.setOrderNumber(order.getOrderNumber());
+            orderPlacedEvent.setEmail(orderRequest.userDetails().email());
+            orderPlacedEvent.setFirstName(orderRequest.userDetails().firstName());
+            orderPlacedEvent.setLastName(orderRequest.userDetails().lastName());
+            log.info("Start - Sending OrderPlacedEvent {} to Kafka topic order-placed", orderPlacedEvent);
+            kafkaTemplate.send("order-placed", orderPlacedEvent);
+            log.info("End - Sending OrderPlacedEvent {} to Kafka topic order-placed", orderPlacedEvent);
         } else {
             throw new RuntimeException("Product with SkuCode "+ orderRequest.skuCode()+ " is not in stock");
         }
